d9c625dba130700215af05aa3d0b18014992cc43
suresh2k02 <suresh2k02@gmail.com>
2021-12-09 14:37:19 +0530

    congif

--- job_portal/job_portal/settings.py
+++ job_portal/job_portal/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-207vo2+dy=rwjw8_om+w%vlt*e--5sh20--jo+j6iol2!ab+u%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- job_portal/job_portal/urls.py
+++ job_portal/job_portal/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('jobapp.urls')),
-    path('',include('account.urls')),
+    path('jobs',include('jobapp.urls')),
+    path('acc',include('account.urls')),
     
 ]
